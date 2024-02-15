e701d91ae4ebb3db81e742340137d9061cc7cca1
Marc <ninjaz.gg@outlook.com>
2024-02-14 22:20:37 -0500

    implement book grpc method

--- src/Novelog.BookService.Api/Services/BookService.cs
+++ src/Novelog.BookService.Api/Services/BookService.cs
@@ -1,6 +1,16 @@
+using Grpc.Core;
+
 namespace Novelog.BookService.Api.Services;
 
 public class BookService : BookServiceDefinition.BookServiceDefinitionBase
 {
-    
+    public override Task<CreateBookResponse> CreateBookUnary(CreateBookRequest request, ServerCallContext context)
+    {
+        // validate & persist book
+
+        return Task.FromResult(new CreateBookResponse
+        {
+            Id = Guid.NewGuid().ToString()
+        });
+    }
 }
