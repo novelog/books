13458c1732882c0f58fa55bd81b68e48a43bc424
Marc <ninjaz.gg@outlook.com>
2024-02-18 08:03:58 -0500

    configure serilog; now logs grpc endpoints

--- src/Novelog.BookService.Api/Program.cs
+++ src/Novelog.BookService.Api/Program.cs
@@ -1,9 +1,11 @@
 using Novelog.BookService.Api.Services;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((ctx, cfg) =>
+    cfg.ReadFrom.Configuration(ctx.Configuration));
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 app.MapGrpcService<BookService>();
-
 app.Run();
