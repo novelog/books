ac088b62b0291bd64d883b94782774698d48986e
Marc <ninjaz.gg@outlook.com>
2024-02-18 09:27:31 -0500

    implement clientStream rpc method

--- src/Novelog.BookService.Api/Services/BookService.cs
+++ src/Novelog.BookService.Api/Services/BookService.cs
@@ -4,7 +4,11 @@ namespace Novelog.BookService.Api.Services;
 
 public class BookService : BookServiceDefinition.BookServiceDefinitionBase
 {
-    public override Task<CreateBookResponse> CreateBookUnary(CreateBookRequest request, ServerCallContext context)
+    private readonly ILogger<BookService> _logger;
+
+    public BookService(ILogger<BookService> logger) => _logger = logger;
+
+    public override Task<CreateBookResponse> CreateBook(CreateBookRequest request, ServerCallContext context)
     {
         // validate & persist book
 
@@ -12,5 +16,23 @@ public class BookService : BookServiceDefinition.BookServiceDefinitionBase
         {
             Id = Guid.NewGuid().ToString()
         });
+    }
+
+    public override async Task<CreateBooksResponse> CreateBooks(
+        IAsyncStreamReader<CreateBookRequest> requestStream,
+        ServerCallContext context)
+    {
+        int totalAdded = 0;
+        while (await requestStream.MoveNext())
+        {
+            var req = requestStream.Current;
+            _logger.LogInformation("received book: {@Book}", req);
+            totalAdded++;
+        }
+
+        return new CreateBooksResponse
+        {
+            TotalAdded = totalAdded
+        };
     }
 }
